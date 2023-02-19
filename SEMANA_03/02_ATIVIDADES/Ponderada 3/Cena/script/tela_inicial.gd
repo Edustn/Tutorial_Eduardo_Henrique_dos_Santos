d218a15905aa8bf11fd55cd5ae8a8866d025368b
Edustn <eduardo.santos@sou.inteli.edu.br>
2023-02-19 18:43:02 -0300

    Exercícios feitos

---
+++ SEMANA_03/02_ATIVIDADES/Ponderada 3/Cena/script/tela_inicial.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var dado1
+var dado2
+var dado3
+var resultado
+
+func _on_Button_pressed():
+#	dado1 = float($dado1.text)
+#	dado2 = float($dado2.text)
+#	dado3 = float($dado3.text)
+#	resultado = dado1 + dado2 + dado3
+	$ColorRect/reposta.text = listaPreenchida()
+
+func _on_Button2_pressed(): #botao para ver a lista que o usuario inseriu
+	$ColorRect/reposta.text = listaUsuario()
+
+func _on_Button3_pressed():
+	$ColorRect/reposta.text = retornarTextoNaTela()
+
+func _on_Button4_pressed():
+	$ColorRect/reposta.text = valor()
+
+
+func listaPreenchida():  # lista prencida
+	var vetor = ["Eduardo","Tommy","Rafa", "Fernando","Ivan", "Luiza", "Ana", "Gustavo"]
+	var nomes = ""
+	
+	for i in range(vetor.size()):
+		nomes += vetor[i] + "\n" 
+	return nomes
+	
+func listaUsuario(): #lsita que o usuario insere os dados
+	var dado = $dado1.text.split(" ", true) # lista em branco que faz a separacao por espaços
+	var list = ""
+	for i in range(dado.size()):
+		list += dado[i] + "\n"
+	return list
+	
+func retornarTextoNaTela(): #funcao para retornar o texto
+	return "Hello, World!"
+
+
+func valor():
+	return $dado2.text 
+
+
+
+
+
